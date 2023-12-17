edb3f4fd04d97a0188b9ce602b310e9d1a652f67
Mantas Mozuraitis <mozmantas@gmail.com>
2023-12-17 13:26:13 +0000

    Implemented registration functionality

--- index.js
+++ index.js
@@ -19,24 +19,85 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {route:"login"});
 })
 
+// GET USER ACCOUNT PAGE
+app.get("/myaccount/:id",async (req,res)=>{
+    res.render("index.ejs", {user: await getUser(req.params.id)});
+})
+
+// USER LOGIN POST
 app.post("/login", async (req,res) => {
-    const e_username = req.body.username;
-    const e_password = req.body.password;
-    
     try {
-        const user = await db.query("SELECT * FROM users WHERE username = $1 AND password = $2", [e_username, e_password]);
-        if (user.rows.length != 0) {
-            console.log(user.rows);
+        const user = await existingUser(req.body.username, req.body.password);
+        if (user.isValidUser) {
+            res.redirect(`/myaccount/${user.userData.id}`);
+        }else{
+            res.render("index.ejs", {message: "User does not exist", route:"login"});
         }
-        res.redirect("/");
     } catch (error) {
-        console.log("user not found");
+        console.error("Error:", error.message);
+        res.render("index.ejs", {message: "An error occurred during login", route:"login"});
     }
 })
+// USER LOGIN GET 
+app.get("/login", (req,res)=>{
+    res.render("index.ejs", {route:"login"});
+})
+
+
+
+// USER REGISTER POST 
+app.post("/register", async (req,res)=>{
+    try {
+        const user = await registerUser(req.body.username, req.body.password);
+        res.render("index.ejs", {route:"login"});
+    } catch (error) {
+        console.error("Error:", error.message);
+        res.render("index.ejs", {message: "Register error", route:"register"});
+    }
+})
+// USER REGISTER GET 
+app.get("/register", (req,res)=>{
+    res.render("index.ejs", {route:"register"});
+})
+
+
+
 
 app.listen(port, (req,res)=>{
     console.log(`Server is listening on port: ${port}`);
 })
+
+
+// FUNCTIONS 
+
+async function existingUser(username, password) {
+    try {
+        const res = await db.query("SELECT * FROM users WHERE username = $1 AND password = $2",[username, password]);
+        return res.rowCount>0?{isValidUser:true,userData:res.rows[0]}:{isValidUser:false};
+    } catch (error) {
+        console.error("Error:", error.message);
+        return false;
+    }
+}
+
+async function getUser(id){
+    try {
+        const res = await db.query("SELECT * FROM users WHERE id = $1",[id]);
+        return res.rows[0];
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+async function registerUser(username, password){
+    try {
+        const res = await db.query("INSERT INTO users (username, password) VALUES ($1,$2)",[username, password]);
+        console.log("New user has been registered:");
+        console.log(res);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
