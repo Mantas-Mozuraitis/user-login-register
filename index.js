af25e39213fb5d767253e6402d1c999812a7c800
Mantas Mozuraitis <mozmantas@gmail.com>
2023-12-18 15:38:02 +0000

    implement passport and bcrypt modules to log user session and encrypt password

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+import { config } from 'dotenv';
+if (process.env.NODE_ENV !== "production"){
+    config();
+}
+
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import flash from "express-flash";
+import session from "express-session";
+import {initialize} from "./passport-config.js";
+
+initialize(passport,username => getUserByUsername(username), id => getUser(id));
 
 const app = express();
 const port = 3000;
@@ -17,30 +29,53 @@ db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized: false,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req,res) => {
     res.render("index.ejs", {route:"login"});
 })
 
 // GET USER ACCOUNT PAGE
-app.get("/myaccount/:id",async (req,res)=>{
-    res.render("index.ejs", {user: await getUser(req.params.id)});
+app.get("/myaccount",async (req,res)=>{
+    res.render("index.ejs", {user:req.user});
 })
 
 // USER LOGIN POST
-app.post("/login", async (req,res) => {
-    try {
-        const user = await existingUser(req.body.username, req.body.password);
-        if (user.isValidUser) {
-            res.redirect(`/myaccount/${user.userData.id}`);
-        }else{
-            res.render("index.ejs", {message: "User does not exist", route:"login"});
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
-        res.render("index.ejs", {message: "An error occurred during login", route:"login"});
-    }
-})
+app.post("/login", passport.authenticate("local", {
+    successRedirect:"/myaccount",
+    failureRedirect:"/login",
+    failureFlash:true,
+}))
+
+// app.post("/login", async (req,res) => {
+//     const username_input = req.body.username;
+//     try {
+//         const user = await existingUser(username_input, req.body.password);
+//         if (user.isValidUser) {
+//             res.redirect(`/myaccount/${user.userData.id}`);
+//         }else{
+//             try {
+//                 await existingUsername(username_input)
+//                 ?res.render("index.ejs", {message: "Entered password is not correct, try again", route:"login"})
+//                 :res.render("index.ejs", {message: "User does not exist, please register", route:"login"});
+//             } catch (error) {
+//                 console.error("Error:", error.message);
+//             }
+//         }
+//     } catch (error) {
+//         console.error("Error:", error.message);
+//         res.render("index.ejs", {message: "An error occurred during login", route:"login"});
+//     }
+// })
+
+
 // USER LOGIN GET 
 app.get("/login", (req,res)=>{
     res.render("index.ejs", {route:"login"});
@@ -50,14 +85,15 @@ app.get("/login", (req,res)=>{
 
 // USER REGISTER POST 
 app.post("/register", async (req,res)=>{
+    const username_input = req.body.username;
     try {
-        const user = await registerUser(req.body.username, req.body.password);
+        const user = await registerUser(username_input, req.body.password, req.body.re_password);
         user.userRegistered
         ?res.render("index.ejs", {route:"login", message:user.message})
-        :res.render("index.ejs", {route:"register", message:user.message}); 
+        :res.render("index.ejs", {route:"register", message:user.message,reg_username:username_input}); 
     } catch (error) {
         console.error("Error:", error.message);
-        res.render("index.ejs", {message: "Register error", route:"register"});
+        res.render("index.ejs", {message: "Register error", route:"register",reg_username:username_input});
     }
 })
 // USER REGISTER GET 
@@ -71,6 +107,8 @@ app.get("/register", (req,res)=>{
 app.listen(port, (req,res)=>{
     console.log(`Server is listening on port: ${port}`);
 })
+
+
 
 
 // FUNCTIONS 
@@ -91,19 +129,26 @@ async function getUser(id){
         return res.rows[0];
     } catch (error) {
         console.error("Error:", error.message);
+        return null;
     }
 }
 
-async function registerUser(username, password){
+async function registerUser(username, password, re_password){
     const existing_username = await existingUsername(username);
     if (!existing_username) {
-        try {
-            const res = await db.query("INSERT INTO users (username, password) VALUES ($1,$2)",[username, password]);
-            console.log("New user has been registered:");
-            return {userRegistered:true};
-        } catch (error) {
-            console.error(error.message);
-            return {userRegistered:false};
+        if (password === re_password) {
+            try {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                await db.query("INSERT INTO users (username, password) VALUES ($1,$2)",[username, hashedPassword]);
+                console.log("New user has been registered");
+                return {userRegistered:true};
+            } catch (error) {
+                console.error(error.message);
+                return {userRegistered:false};
+            }
+        }else{
+            console.log("passwords do not match");
+            return {userRegistered:false,message:"passwords do not match"};
         }
     }else{
         console.log("username already exists");
@@ -118,5 +163,15 @@ async function existingUsername(username){
     } catch (error) {
         console.error("Error:", error.message);
         return false;
+    }
+}
+
+async function getUserByUsername(username){
+    try {
+        const user = await db.query("SELECT * FROM users WHERE username = $1", [username]);
+        return user.rows[0];
+    } catch (error) {
+        console.error("Error:", error.message);
+        return null;
     }
 }
---
+++ passport-config.js
@@ -0,0 +1,32 @@
+import passportLocal from "passport-local";
+import bcrypt from "bcrypt";
+
+const localStrategy = passportLocal.Strategy;
+
+export function initialize(passport, getUserByUsername, getUser){
+
+    const authenticateUser = async (username, password, done) => {
+        const user = await getUserByUsername(username);
+        console.log(password);
+        console.log(user.password);
+        if (user == null) {
+            return done(null, false, {message:"No user with that username"});
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null,user);
+            }else{
+                return done(null, false, {message:"Passwpord incorrect"});
+            }
+        } catch (error) {
+            return done (error);
+        }
+    }
+
+    passport.use(new localStrategy(authenticateUser));
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((user, done) => {
+        return done(null, getUser(user.id));
+    });
+}
